0124168ca19a39e5decd9845e22614cf38ff9576
pomnom <yuslibra6@gmail.com>
2021-08-04 15:44:03 +0800

    produk 1

---
+++ js.js
@@ -0,0 +1,24 @@
+$("#collapsible").click(function () {
+    $("#x").toggle("slow");
+    $("#p").toggle("slow");
+});
+$("#return").click(function () {
+    $("#p").toggle("slow");
+    $("#x").toggle("slow");
+});
+$("#collapsible1").click(function () {
+    $("#x1").toggle("slow");
+    $("#p1").toggle("slow");
+});
+$("#return1").click(function () {
+    $("#p1").toggle("slow");
+    $("#x1").toggle("slow");
+});
+$("#collapsible2").click(function () {
+    $("#x2").toggle("slow");
+    $("#p2").toggle("slow");
+});
+$("#return2").click(function () {
+    $("#p2").toggle("slow");
+    $("#x2").toggle("slow");
+});
